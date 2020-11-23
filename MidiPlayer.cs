ef30b82fb8e4602b8cd73ceb5d1cc7faf5dc4d5e
cepthomas <cepthomas@gmail.com>
2020-11-22 18:21:01 -0600

    WIP.

--- MidiPlayer.cs
+++ MidiPlayer.cs
@@ -23,17 +23,15 @@ using NBagOfTricks.Utils;
 // DeltaTicksPerQuarterNote (ppq): 96
 // 100 bpm = 9,600 ticks/min = 160 ticks/sec = 0.16 ticks/msec = 6.25 msec/tick
 
+// If we use ppq of 8 for 32nd notes:
+// 100 bpm = 800 ticks/min = 13.33 ticks/sec = 0.01333 ticks/msec = 75.0 msec/tick
+//  99 bpm = 792 ticks/min = 13.20 ticks/sec = 0.0132 ticks/msec  = 75.757 msec/tick
+
 
 // Nebulator:
 // <summary>Subdivision setting. 4 means 1/16 notes, 8 means 1/32 notes.</summary>
 // public const int SUBBEATS_PER_BEAT = 4;
 // 100 bpm = 
-//void SetSpeedTimerPeriod()
-//{
-//    double secPerBeat = 60 / potSpeed.Value; 0.6
-//    double msecPerTick = 1000 * secPerBeat / Time.SUBBEATS_PER_BEAT; 150
-//    _timer.SetTimer("NEB", (int)msecPerTick);
-//}
 
 // TODOC (+TimeBar) display bar.beat like 34.1:909  34.2:123  34.3:456  34.4:777  Time is like 00.00:000
 // new slders.
@@ -45,10 +43,22 @@ namespace ClipExplorer
 {
     public partial class MidiPlayer : UserControl, IPlayer
     {
-        #region Fields
+        #region Constants
         /// <summary>Midi caps.</summary>
         const int NUM_CHANNELS = 16;
 
+        /// <summary>Only 4/4 time supported.</summary>
+        const int BEATS_PER_BAR = 4;
+
+        /// <summary>Subdivision setting aka resolution. 4 means 1/16 notes, 8 means 1/32 notes, etc.
+        /// Higher is better accuracy relative to original timing.</summary>
+        const int SUBBEATS_PER_BEAT = 8;
+
+        /// <summary>Update time. Compromise between accuracy and resource usage.</summary>
+        const int TIMER_PERIOD = 5;
+        #endregion
+
+        #region Fields
         /// <summary>Indicates whether or not the midi is playing.</summary>
         bool _running = false;
 
@@ -85,8 +95,8 @@ namespace ClipExplorer
         /// <inheritdoc />
         public double CurrentTime //TODOC all these with _stepTime
         {
-           get { return _midiOut == null ? 0 : TicksToTime(_currentTick); }
-           set { if (_midiOut != null) { _currentTick = TimeToTicks(value); } }
+           get { return _midiOut == null ? 0 : SubBeatsToTime(_currentSubBeat); }
+           set { if (_midiOut != null) { _currentSubBeat = TimeToSubBeats(value); } }
         }
 
         /// <inheritdoc />
@@ -118,6 +128,7 @@ namespace ClipExplorer
             _timer = new MmTimerEx();
             SetTempo(100);
             _timer.TimerElapsedEvent += TimerElapsedEvent;
+            _timer.SetTimer("MP", TIMER_PERIOD);
             _timer.Start();
         }
 
@@ -269,8 +280,7 @@ namespace ClipExplorer
 //            _ticksPerTimerPeriod = TimeToTicks(MMTIMER_PERIOD);
 
             double secPerBeat = 60 / sldTempo.Value;
-            double msecPerTick = 1000 * secPerBeat / MidiTime.SUBBEATS_PER_BEAT;
-            _timer.SetTimer("NEB", (int)msecPerTick);
+            double msecPerTick = 1000 * secPerBeat / SUBBEATS_PER_BEAT;
         }
 
         /// <summary>
@@ -351,22 +361,83 @@ namespace ClipExplorer
         /// </summary>
         /// <param name="msec"></param>
         /// <returns></returns>
-        double TimeToTicks(double msec)
+        int TimeToSubBeats(double msec)
         {
-            double ticks = (double)_tempo * _sourceEvents.DeltaTicksPerQuarterNote * msec / 60 / 1000;
-            return ticks;
+            int subBeats = _tempo * _sourceEvents.DeltaTicksPerQuarterNote * msec / 60 / 1000;
+            return subBeats;
         }
 
         /// <summary>
-        /// Convert ticks to time in msec.
+        /// Convert subbeats to time in msec.
         /// </summary>
         /// <returns></returns>
-        double TicksToTime(double ticks)
+        double SubBeatsToTime(int subBeats)
         {
-            double msec = ticks / (double)_tempo / _sourceEvents.DeltaTicksPerQuarterNote * 60 * 1000;
+            double msec = subBeats / (double)_tempo / _sourceEvents.DeltaTicksPerQuarterNote * 60 * 1000;
             return msec;
         }
         #endregion
+
+        int ScaleSubBeats(long midiTime)
+        {
+            // An example midi file:
+            // WICKGAME.MID is 3:45
+            // DeltaTicksPerQuarterNote (ppq): 384 = 
+            // 100 bpm = 38,400 ticks/min = 640 ticks/sec = 0.64 ticks/msec = 1.5625 msec/tick
+            // Length is 144,000 ticks = 3.75 min = 3:45 (yay)
+            // Smallest tick is 4
+
+
+
+            ///// <summary>Only 4/4 time supported.</summary>
+            //const int BEATS_PER_BAR = 4;
+            ///// <summary>Subdivision setting aka resolution. 4 means 1/16 notes, 8 means 1/32 notes.</summary>
+            //const int SUBBEATS_PER_BEAT = 4;
+            ///// <summary>Update time. Compromise between accuracy and resource usage.</summary>
+            //const int TIMER_PERIOD = 5;
+
+        }
+
+        // /// <summary>Total subBeats for the unit of time.</summary>
+        // public int TotalBeats { get { return Bar * BEATS_PER_BAR + Beat; } }
+
+        // /// <summary>Total subBeats for the unit of time.</summary>
+        // public int TotalSubBeats { get { return (Bar * BEATS_PER_BAR + Beat) * SUBBEATS_PER_BEAT + SubBeat; } }
+
+
+        // public MidiTime(int bar, int beat, int subBeat)
+        // {
+        //     if (beat < 0)
+        //     {
+        //         //throw new Exception("Negative value is invalid");
+        //         beat = 0;
+        //     }
+
+        //     if (subBeat >= 0)
+        //     {
+        //         Beat = beat + subBeat / SUBBEATS_PER_BEAT;
+        //         SubBeat = subBeat % SUBBEATS_PER_BEAT;
+        //     }
+        //     else
+        //     {
+        //         subBeat = Math.Abs(subBeat);
+        //         Beat = beat - (subBeat / SUBBEATS_PER_BEAT) - 1;
+        //         SubBeat = SUBBEATS_PER_BEAT - (subBeat % SUBBEATS_PER_BEAT);
+        //     }
+        // }
+
+        // public MidiTime(int subBeats)
+        // {
+        //     if (subBeats < 0)
+        //     {
+        //         throw new Exception("Negative value is invalid");
+        //     }
+
+        //     Bar = subBeats / BEATS_PER_BAR;
+        //     Beat = subBeats % (BEATS_PER_BAR * SUBBEATS_PER_BEAT);
+        //     SubBeat = subBeats % SUBBEATS_PER_BEAT;
+        // }
+
     }
 
     /// <summary>Channel events and other properties.</summary>
@@ -403,4 +474,40 @@ namespace ClipExplorer
         }
 
     }
+
+    /// <summary>
+    /// Container for raw midi event and internal stuff.
+    /// </summary>
+    public class MidiStep
+    {
+        public MidiEvent RawEvent { get; set; } = null;
+
+        /// <summary>The possibly modified Volume.</summary>
+        public double VelocityToPlay { get; set; } = 0.5;
+
+        /// <summary>For viewing pleasure.</summary>
+        // public override string ToString()
+        // {
+        //     return $"channel:{ChannelNumber}";
+        // }
+
+        // public void Adjust(double masterVolume, double channelVolume)
+        // {
+        //     // Maybe alter note velocity.
+        //     if (Device is NOutput)
+        //     {
+        //         NOutput nout = Device as NOutput;
+        //         double vel = Velocity * channelVolume * masterVolume;
+        //         VelocityToPlay = MathUtils.Constrain(vel, 0, 1.0);
+        //     }
+        // }
+
+        // /// <summary>For viewing pleasure.</summary>
+        // public override string ToString()
+        // {
+        //     return $"StepNoteOn: {base.ToString()} note:{NoteNumber:F2} vel:{VelocityToPlay:F2} dur:{Duration}";
+        // }
+
+    }
+
 }
