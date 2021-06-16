d41bb9c640d330106bc252bf46f7e7772f976cb1
cepthomas <cepthomas@gmail.com>
2021-06-16 17:21:16 -0500

    Updated NBOT.

--- MainForm.cs
+++ MainForm.cs
@@ -190,33 +190,7 @@ namespace ClipExplorer
         /// </summary>
         void About_Click(object sender, EventArgs e)
         {
-            // Make some markdown.
-            List<string> mdText = new List<string>();
-
-            // Main help file.
-            mdText.Add(File.ReadAllText(@".\README.md"));
-
-            // Put it together.
-            List<string> htmlText = new List<string>
-            {
-                // Boilerplate
-                $"<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">",
-                $"<style>body {{ background-color: white; font-family: \"Arial\", Helvetica, sans-serif; }}",
-                $"</style></head><body>"
-            };
-
-            // Meat.
-            string mdHtml = string.Join(Environment.NewLine, mdText);
-            htmlText.Add(mdHtml);
-
-            // Bottom.
-            string ss = "<!-- Markdeep: --><style class=\"fallback\">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src=\"markdeep.min.js\" charset=\"utf-8\"></script><script src=\"https://casual-effects.com/markdeep/latest/markdeep.min.js\" charset=\"utf-8\"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility=\"visible\")</script>";
-            htmlText.Add(ss);
-            htmlText.Add($"</body></html>");
-
-            string fn = Path.Combine(Path.GetTempPath(), "ClipExplorer.html");
-            File.WriteAllText(fn, string.Join(Environment.NewLine, htmlText));
-            Process.Start(fn);
+            Tools.MarkdownToHtml(File.ReadAllLines(@".\README.md").ToList(), "lightcyan", "helvetica", true);
         }
 
         /// <summary>
